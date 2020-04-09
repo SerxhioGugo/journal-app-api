1fdfe21a7cc8399ff6ee6baebca185254476e9b6
SerxhioGugo <serxhio.gugo@gmail.com>
2020-04-09 18:23:24 -0400

    add mongoose to mongoDB + colors

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
+//Main starting point of the app
+
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-//Route files
-const posts = require('./routes/posts');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 //Load dotenv files (environment variables)
 dotenv.config({ path: './config/config.env' });
-const PORT = process.env.PORT || 5000;
+
+//Connect to database
+connectDB();
+
+//Route files
+const posts = require('./routes/posts');
 
 const app = express();
 
@@ -20,6 +26,17 @@ app.use('default', morgan);
 //Mount routers
 app.use('/api/v1/posts', posts);
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${process.env.NODE_ENV} mode on port ${PORT}`);
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => {
+  console.log(
+    `Server running on ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  );
+});
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  //Close server & Exit process
+  server.close(() => process.exit(1));
 });
