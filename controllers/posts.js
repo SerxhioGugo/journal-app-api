ff1db7c8e8101b42e6b18679f5f0af16000496d9
SerxhioGugo <serxhio.gugo@gmail.com>
2020-04-09 21:58:02 -0400

    add crud for posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -9,8 +9,13 @@ const Post = require('../models/Post');
 // @route       GET /api/v1/posts
 // @access      Public
 
-exports.getPosts = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show all posts' });
+exports.getPosts = async (req, res, next) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json({ success: true, count: posts.length, data: posts });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 /*--------------------------------------------------------------------------*/
@@ -18,10 +23,18 @@ exports.getPosts = (req, res, next) => {
 // @route       GET /api/v1/posts/:id
 // @access      Public
 
-exports.getPost = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Get post with ID: ${req.params.id}` });
+exports.getPost = async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: post });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 /*--------------------------------------------------------------------------*/
@@ -46,10 +59,21 @@ exports.createPost = async (req, res, next) => {
 // @route       PUT /api/v1/posts/:id
 // @access      Private
 
-exports.updatePost = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update post with ID: ${req.params.id}` });
+exports.updatePost = async (req, res, next) => {
+  try {
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!post) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: post });
+  } catch (err) {
+    return res.status(400).json({ success: false });
+  }
 };
 
 /*--------------------------------------------------------------------------*/
@@ -57,8 +81,16 @@ exports.updatePost = (req, res, next) => {
 // @route       DELETE /api/v1/posts/:id
 // @access      Private
 
-exports.deletePost = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete post with ID: ${req.params.id}` });
+exports.deletePost = async (req, res, next) => {
+  try {
+    const post = await Post.findByIdAndDelete(req.params.id);
+
+    if (!post) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: {} });
+  } catch (err) {
+    return res.status(400).json({ success: false });
+  }
 };
