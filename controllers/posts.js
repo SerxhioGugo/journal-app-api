27420878a63d38d2cf520ab732c05369869059ad
SerxhioGugo <serxhio.gugo@gmail.com>
2020-04-21 22:47:40 -0400

    added pagination

--- controllers/posts.js
+++ controllers/posts.js
@@ -13,14 +13,70 @@ const Post = require('../models/Post');
 
 exports.getPosts = asyncHandler(async (req, res, next) => {
   let query;
-  let queryStr = JSON.stringify(req.query);
 
+  //Copy req.query
+  const reqQuery = { ...req.query };
+
+  //Fields to exclude
+  const removeFields = ['select', 'sort', 'page', 'limit'];
+
+  //Loop over removeFields and delete them rom reqQuery
+  removeFields.forEach(param => delete reqQuery[param]);
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  //Creating possible operators (&gt, gte)
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  // Finding resource
   query = Post.find(JSON.parse(queryStr));
 
-  console.log(req.query);
+  //Select Fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  //Sort by date
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 10;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Post.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
+  //excecuting query
   const posts = await query;
-  res.status(200).json({ success: true, count: posts.length, data: posts });
+
+  // Pagination Result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+
+  res
+    .status(200)
+    .json({ success: true, count: posts.length, pagination, data: posts });
 });
 
 /*--------------------------------------------------------------------------*/
